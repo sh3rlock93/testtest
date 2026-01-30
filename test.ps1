92d07fd577ac8a67ac6033c3d0f7d12b15ad4159
sherlock <sh3rlock93@gmail.com>
2026-01-30 16:04:35 +0900

    Update test.ps1
    ㅅㅅ

--- test.ps1
+++ test.ps1
@@ -1,1 +1,9 @@
-echo "hi"
+Install-Module -Name Posh-SSH
+Import-Module Posh-SSH
+
+$pass = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("root", $pass)
+
+# 터널링 세션 생성
+$session = New-SSHTunnel -ComputerName "pentest.yoobi.kr" -Port 10500 -Credential $credential -LocalPort 8080 -RemotePort 80
+Start-SSHTunnel -SSHTunnel $session
